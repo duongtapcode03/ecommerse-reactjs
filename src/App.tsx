0e9be9020d8a0c602f5468374b3a34ad789546c9
duongtapcode03 <quanxuanduong2409@gmail.com>
2025-06-20 10:47:45 +0700

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      Ecommerse
-    </div>
-  )
+    return <div className='App'>Ecommerse</div>;
 }
 
-export default App
+export default App;
